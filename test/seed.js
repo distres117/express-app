c796a03de34ffacbfc6b67705195feca5add8520
distres117 <omcrobbie@gmail.com>
2016-02-26 23:51:19 +0000

    better seeding

--- test/seed.js
+++ test/seed.js
@@ -1,42 +1,16 @@
-var User = require('../models/user.js'),
-    Todo = require('../models/todo.js'),
-    ObjectId = require('mongoose').Types.ObjectId;
+var RefArray= require('../utils/ref-array');
 
-function RefArray(arr,fn){
-    for (var i in arr)
-        this.objectIds.push(fn());
-    this.arr = arr;
-}
-
-RefArray.prototype.objectIds = [];
-
-RefArray.prototype.build = function(fields){
-    var oids = this.objectIds;
-  for (var obj of this.arr){
-    for(var field in obj){
-        if (fields.indexOf(field) > -1)
-            obj[field] = Array.isArray(obj[field]) ? obj[field].map(i=>oids[i]) : oids[obj[field]];
-    }
-  }
-  return this.arr;
-}
-
-var _todos = new RefArray([{
+var _todos = new RefArray('Todo',[{
     content: "Feed the dog", user: 0, _id: 2 }, 
     {content: "Water the plants", user: 0, _id: 3 }, 
-    {content: "Clean the house", user: 1 , _id: 4 }], ()=>ObjectId());
+    {content: "Clean the house", user: 1 , _id: 4 }]);
 
-var _users = new RefArray([
+var _users = new RefArray('User',[
     {name: "Oliver", email: "omcrobbie@gmail.com", _id:0 , todos: [2,3] },
     {name: "Andrew", email: "apmcrobbie@gmail.com", _id:1 , todos: [4] }
-    ], ()=>ObjectId());
-
+    ]);
 
 module.exports = {
-    todos: _todos.build(['_id', 'user']).map(function(todo){
-        return new Todo(todo);
-    }),
-    users: _users.build(['_id', 'todos']).map(function(user){
-        return new User(user);  
-    })
+    todos: _todos.build(['_id', 'user']),
+    users: _users.build(['_id', 'todos']) 
 };
